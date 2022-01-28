64a443d0c9afc90d6183ee47828352a1b28a4168
karinababii <karino4ka201351@gmail.com>
2022-01-28 12:29:41 +0200

    testt

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,4 +1,10 @@
+/// <reference types="cypress"/>
 describe('Work with', () => {
+
+  let userone
+  let usertwo
+  let userthr
+  let userfour
 
     it('slider', () => {
         cy.visit('http://the-internet.herokuapp.com/horizontal_slider')
@@ -25,4 +31,42 @@ describe('Work with', () => {
       cy.get('#file-submit').click()
       cy.get('#uploaded-files').contains('bee-on-daisy.jpg')
     })
+
+    it('status codes', () => {
+      cy.visit('https://the-internet.herokuapp.com/status_codes')
+      cy.request('https://the-internet.herokuapp.com/status_codes/200').then((response) => {
+        expect(response.status).to.eq(200)}),
+      cy.request('https://the-internet.herokuapp.com/status_codes/301').then((response) => {
+        expect(response.status).to.eq(301)})
+      /*cy.request('https://the-internet.herokuapp.com/status_codes/404').then((response) => {
+          expect(response.status).to.eq(404)}),
+      cy.request('https://the-internet.herokuapp.com/status_codes/500').then((response) => {
+        expect(response.status).to.eq(500)})
+      */
+    })
+
+    it ('table data', () => {
+      cy.visit('https://the-internet.herokuapp.com/tables#delete')
+      cy.get('#table1 > tbody > tr:nth-child(1) > td:nth-child(3)').then(($td) => {
+        userone = $td.text();
+        cy.log(userone);
+      })
+      
+      cy.get('#table1 > tbody > tr:nth-child(2) > td:nth-child(3)').then(($td) => {
+        usertwo = $td.text();
+        cy.log(usertwo);
+      })
+
+      cy.get('#table1 > tbody > tr:nth-child(3) > td:nth-child(3)').then(($td) => {
+        userthr = $td.text();
+        cy.log(userthr);
+      })
+
+      cy.get('#table1 > tbody > tr:nth-child(4) > td:nth-child(3)').then(($td) => {
+        userfour = $td.text();
+        cy.log(userfour);
+      })
+
+    })
+
 })
